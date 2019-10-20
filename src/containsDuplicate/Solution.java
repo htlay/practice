4ad82df4ec880b96f2964c1f1a2ed278a74044ce
Hung Lay <hunglay0420@gmail.com>
2019-10-20 13:07:40 -0700

    contains duplicate

---
+++ src/containsDuplicate/Solution.java
@@ -0,0 +1,44 @@
+/*
+    217. Contains Duplicate
+    Given an array of integers, find if the array contains any duplicates.
+
+    Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+    Example 1:
+
+    Input: [1,2,3,1]
+    Output: true
+    Example 2:
+
+    Input: [1,2,3,4]
+    Output: false
+    Example 3:
+
+    Input: [1,1,1,3,3,4,3,2,4,2]
+    Output: true
+ */
+
+package containsDuplicate;
+
+import java.util.HashSet;
+
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<nums.length; i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        Solution ss = new Solution();
+        int [] arr = {1,2,3,1};
+        System.out.println(ss.containsDuplicate(arr));
+    }
+}
