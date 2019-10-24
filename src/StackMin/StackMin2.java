98f5f419805665be13264a703655417466a2cdc8
Hung Lay <hunglay0420@gmail.com>
2019-10-24 13:09:07 -0700

    update

--- src/StackMin/StackMin2.java
+++ src/StackMin/StackMin2.java
@@ -1,41 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 
  */
 package StackMin;
 
 import java.util.Stack;
 
 public class StackMin2 extends Stack<Integer> {
-//    Stack<Integer> s2;
-//    public StackMin2(){
-//        s2 = new Stack<>();
-//    }
-//    
-//    public void push(int value){
-//        if(value<=min()){
-//            s2.push(value);
-//        }
-//        super.push(value);
-//    }
-//    
-//    public Integer pop(){
-//        int value = super.pop();
-//        if(value == min()){
-//            s2.pop();
-//        }
-//        return value;
-//    }
-//    
-//    public int min(){
-//        if(s2.isEmpty()){
-//            return Integer.MAX_VALUE;
-//        }
-//        else{
-//            return s2.peek();
-//        }
-//    }
+    
     private Stack<Integer> s2;
     
     public StackMin2(){
@@ -58,6 +29,11 @@ public class StackMin2 extends Stack<Integer> {
     }
 
     public Integer min() {
-        return s2.peek();
+        if(s2.isEmpty()){
+            return Integer.MAX_VALUE;
+        }
+        else{
+            return s2.peek();
+        }
     }
 }
